80bc6d531087b3292860d1c4f672c92577a6e9e6
Jakie Khan <khanjakie651@gmail.com>
2025-04-10 07:50:44 +0545

    New

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 let cl=document.querySelector(".check");
+let original=document.querySelector(".one").innerHTML
 
 cl.addEventListener("click",() =>{
-    let per = Math.floor(Math.random() * (100 - 80 + 1)) + 80;
     
+    let per = Math.floor(Math.random() * (100 - 80 + 1)) + 80;
     if (document.querySelector(".names1").value=="" || document.querySelector(".names2").value=="")
         {
             document.querySelector(".alert").style.display="block";
             document.querySelector(".one").innerHTML="Please enter the name";
+            document.querySelector(".per").style.display="none";
         }
         else
         {
             document.querySelector(".alert").style.display="block";
+            document.querySelector(".one").innerHTML =original;
+            document.querySelector(".per").style.display="block";
             document.querySelector(".per").innerHTML=per + "%";
         
     }
